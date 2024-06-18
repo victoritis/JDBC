92c2ed172362b1e5c201941201e8a66b6063f005
victoritis <v.gdelcampo@udc.es>
2024-06-18 19:10:41 +0200

    Añadida dependencia

--- PrácticaEvaluable_JDBC_BilleteTren_2C/src/lsi/ubu/servicios/ServicioImpl.java
+++ PrácticaEvaluable_JDBC_BilleteTren_2C/src/lsi/ubu/servicios/ServicioImpl.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Time;
 import java.util.Date;
-
+import lsi.ubu.excepciones.CompraBilleteTrenException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
