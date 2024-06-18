b1b5aa5fbdbc6e30b2314546aab96e09c48b8833
victoritis <v.gdelcampo@udc.es>
2024-06-18 19:19:02 +0200

    Reestructuracion el proyecto

--- PrácticaEvaluable_JDBC_BilleteTren_2C/src/lsi/ubu/excepciones/CompraBilleteTrenException.java
+++ PrácticaEvaluable_JDBC_BilleteTren_2C/src/lsi/ubu/excepciones/CompraBilleteTrenException.java
@@ -27,20 +27,13 @@ public class CompraBilleteTrenException extends SQLException {
 	private String mensaje;
 
 	public CompraBilleteTrenException(int code) {
-		this.codigo = code;
-		switch (code) {
-			case NO_PLAZAS:
-				this.mensaje = "No hay plazas suficientes.";
-				break;
-			case NO_EXISTE_VIAJE:
-				this.mensaje = "No existe viaje para tal fecha, hora, origen y destino.";
-				break;
-			default:
-				this.mensaje = "Error desconocido.";
-		}
+		/*
+		 * A completar por el alumno
+		 */
+
 		LOGGER.debug(mensaje);
 
-		// Traza de pila
+		// Traza_de_pila
 		for (StackTraceElement ste : Thread.currentThread().getStackTrace()) {
 			LOGGER.debug(ste.toString());
 		}
