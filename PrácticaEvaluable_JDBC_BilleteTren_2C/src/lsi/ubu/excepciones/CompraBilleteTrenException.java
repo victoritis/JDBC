1549249b27fa39d3b03f699401606a5c0b87b132
victoritis <v.gdelcampo@udc.es>
2024-06-18 19:39:39 +0200

    ComprarBillete cambiado

--- PrácticaEvaluable_JDBC_BilleteTren_2C/src/lsi/ubu/excepciones/CompraBilleteTrenException.java
+++ PrácticaEvaluable_JDBC_BilleteTren_2C/src/lsi/ubu/excepciones/CompraBilleteTrenException.java
@@ -27,9 +27,17 @@ public class CompraBilleteTrenException extends SQLException {
 	private String mensaje;
 
 	public CompraBilleteTrenException(int code) {
-		/*
-		 * A completar por el alumno
-		 */
+		this.codigo = code;
+		switch (code) {
+			case NO_PLAZAS:
+				this.mensaje = "No hay plazas suficientes.";
+				break;
+			case NO_EXISTE_VIAJE:
+				this.mensaje = "No existe viaje para tal fecha, hora, origen y destino.";
+				break;
+			default:
+				this.mensaje = "Excepción no identificada.";
+		}
 
 		LOGGER.debug(mensaje);
 
